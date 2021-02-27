78cc32e4f905605157e5949dd266ee5520a2f94f
nomrand <42882840+nomrand@users.noreply.github.com>
2021-02-27 16:29:46 +0700

    add template (sample http/https server)

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # js_oauth
+
+
+
+## HTTP
+
+ python -m http.server 8000
+
+ http://localhost:8000
+
+
+
+## HTTPS
+
+ python https.py
+
+ https://localhost:4443
+
+* in shell (such as git-bash)
+
+ openssl req -new -x509 -keyout localhost.pem -out localhost.pem -days 365 -nodes
---
+++ https.py
@@ -0,0 +1,11 @@
+import http.server, ssl
+
+server_address = ('localhost', 4443)
+httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket,
+                               server_side=True,
+                               certfile='localhost.pem',
+                               ssl_version=ssl.PROTOCOL_TLS)
+
+print(f'start server https://{server_address[0]}:{server_address[1]}')
+httpd.serve_forever()
